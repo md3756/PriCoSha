9035e50de7f4fa9c8b0e80e5288079a9bb8796ce
lyannel <ll3325@nyu.edu>
2018-11-20 18:44:59 -0500

    home page, select blogger, show posts

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pymysql
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello"
+def home():
+    return render_template('home.html')
 
 @app.route('/login')
 def login():
@@ -14,6 +14,13 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/select_blogger')
+def select_blogger():
+    return render_template('select_blogger.html')
+
+@app.route('/show_posts')
+def show_posts():
+    return render_template('show_posts.html')
 
 if __name__ == "__main__":
     app.run('127.0.0.1', 5000, debug=True)
